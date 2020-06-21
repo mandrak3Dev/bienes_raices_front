79089253a85f588734571e0fc955921ae4a0fd1c
Mau Orozco <maudob@live.com>
2020-06-20 22:00:43 -0500

    Setup gatsby and integration with strapi

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,49 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import { css } from "@emotion/core"
+// import styled from "@emotion/styled"
+import { Link, useStaticQuery, graphql } from "gatsby"
+import Navegacion from "./navegacion"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+// const EnlaceHome = styled(Link)`
+//   color: #fff;
+//   text-align: center;
+//   text-decoration: none;
+// `
+
+const Header = ({ siteName }) => {
+
+  const { logo } = useStaticQuery(graphql`
+    query {
+      logo: file(relativePath: { eq: "logo.svg" }) {
+        publicURL
+      }
+    }
+  `)  
+
+  return (
+    <header
+      css={css`
+        background-color: #0d283b;
+        padding: 1rem;
+      `}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+      <div
+        css={css`
+          max-width: 120rem;
+          margin: 0 auto;
+          text-align: center;
+          @media (min-width: 768px) {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+          }
+        `}
+      >
+        <Link to="/"><img src={logo.publicURL} alt="Logotipo bienes raices"/></Link>
+        <Navegacion />
+      </div>
+    </header>
+  )
 }
 
 export default Header
---
+++ src/hooks/useInicio.js
@@ -0,0 +1,30 @@
+import { graphql, useStaticQuery } from "gatsby"
+
+const useInicio = () => {
+  const resultado = useStaticQuery(graphql`
+    query {
+      allStrapiPaginas(filter: { nombre: { eq: "Inicio" } }) {
+        nodes {
+          id
+          nombre
+          contenido
+          imagen {
+            sharp: childImageSharp {
+              fluid(maxWidth: 1200) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return resultado.allStrapiPaginas.nodes.map(inicio => ({
+    nombre: inicio.nombre,
+    contenido: inicio.contenido,
+    imagen: inicio.imagen,
+  }))
+}
+
+export default useInicio
